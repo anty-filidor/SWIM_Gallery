10500b201e3028f41d2e574ef57b4eb79c302ade
michal <michal.czuba.1995@gmail.com>
2019-04-20 16:28:03 +0200

    added toolbar

--- app/src/main/java/com/example/gallery/GalleryActivity.java
+++ app/src/main/java/com/example/gallery/GalleryActivity.java
@@ -9,6 +9,11 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class GalleryActivity extends AppCompatActivity {
@@ -17,6 +22,14 @@ public class GalleryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_recycler);
+
+        //create toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(R.string.app_name);
+        }
 
 
         //messages from start activity. They determine layout and onclickitem listener
@@ -24,14 +37,14 @@ public class GalleryActivity extends AppCompatActivity {
         final String intentMode = fromStartActivity.getStringExtra("intentChoice");
         String layoutMode = fromStartActivity.getStringExtra("layoutChoice");
 
-        setContentView(R.layout.activity_gallery_recycler);
 
+        //create recycler view to display gallery
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
-
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 1);
         recyclerView.setLayoutManager(layoutManager);
 
+        //view accurate layout (in adapter) to user preferences
         GalleryRecyclerAdapter adapter;
         if(layoutMode.equals("Grid")) {
             adapter = new GalleryRecyclerAdapter(getApplicationContext(), getData(), intentMode, R.layout.item_layout_grid);
@@ -42,6 +55,7 @@ public class GalleryActivity extends AppCompatActivity {
         }
         recyclerView.setAdapter(adapter);
     }
+
 
     private ArrayList getData() {
         final ArrayList imageItems = new ArrayList();
@@ -54,5 +68,24 @@ public class GalleryActivity extends AppCompatActivity {
             imageItems.add(new ImageItem(imagePath, imageFiles[i].getName()));
         }
         return imageItems;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.manu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.folder:
+                Toast.makeText(this, "You clicked about", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/gallery/StartActivity.java
+++ app/src/main/java/com/example/gallery/StartActivity.java
@@ -3,6 +3,7 @@ package com.example.gallery;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -28,6 +29,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
 
         textView1 =(TextView)findViewById(R.id.textView1);
         textView1.setMovementMethod(LinkMovementMethod.getInstance());
